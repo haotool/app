c5177f839d84fbfed3f2fcd75795594de7a9b514
haotool <haotool.org@gmail.com>
2025-12-25 13:22:18 +0800

    fix(ratewise): 新增 vitest-dom.d.ts 類型聲明以修復 CI 錯誤

---
+++ apps/ratewise/src/vitest-dom.d.ts
@@ -0,0 +1,9 @@
+/**
+ * Vitest 與 @testing-library/jest-dom 類型聲明
+ *
+ * 此文件擴展 Vitest 的 expect 以包含 jest-dom matchers
+ * 參考: https://testing-library.com/docs/ecosystem-jest-dom/#with-vitest
+ *
+ * @since 2025-12-25
+ */
+import '@testing-library/jest-dom/vitest';
